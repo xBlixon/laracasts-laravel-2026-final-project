ee047de5baa37fc20615ce9f1a9a5830d5932445
Xawier Słupik <48760619+xBlixon@users.noreply.github.com>
2026-05-20 17:50:47 +0200

    fading flash message with alpinejs

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -5,13 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Idea</title>
 </head>
 <body class="bg-background text-foreground">
     <x-layout.nav />
-<main class="max-w-7xl mx-auto px-6">
-    {{ $slot }}
-</main>
+    <main class="max-w-7xl mx-auto px-6">
+        {{ $slot }}
+    </main>
+    @session('success')
+        <div
+            x-data="{ show: true }"
+            x-show="show"
+            x-init="setTimeout(() => show = false, 3000)"
+            x-transition.opacity.duration.300ms
+            class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">{{ $value }}</div>
+    @endsession
 </body>
 </html>
